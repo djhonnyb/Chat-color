83e4e33b957f548e1da4e095c1085dd5f43b4b01
djhonnyb <63619729+djhonnyb@users.noreply.github.com>
2024-11-16 18:14:50 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+import ora from "ora";
+import chalk from "chalk";
+import clear from "console-clear";
+import figlet from "figlet";
+import qrcode from "qrcode-terminal";
+import {
+  makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason,
+} from "@whiskeysockets/baileys";
+import pino from "pino";
+import fs from "fs-extra";
+
+const logger = pino({
+  level: "silent",
+});
+
+const spinner = ora("Iniciando...").start();
+
+const showBanner = () => {
+  clear();
+
+  const program_name = "Mensaje a color";
+
+  const author =
+    chalk.yellow("\nCódigo fuente: ") +
+    chalk.underline.greenBright("https://t.me/djhonnyb\n");
+
+  const howToUseEn =
+    chalk.magenta.bold("Cómo usar:\n") +
+    chalk.blueBright(
+      `Once the QR code is scanned and connected to your WhatsApp account, you can send any text message.
+To trigger the hidetag, send a message to a group containing any emoji.\n`
+    );
+
+  const howToUseId =
+    chalk.magenta.bold("Cómo usar:Una vez que el código QR se escanea y se conecta a tu cuenta de WhatsApp, puedes enviar cualquier mensaje de texto.
+Para activar el mensaje a color, envía un mensaje por el chat.\n") +
+    chalk.blueBright ( `\n` );
+console.log(program_name);
+console.log(author);
+console.log(howToUseEn);
+console.log(howToUseId);
+};
+const sendMessage = (message, groupJid) => {
+if (message.message.extendedTextMessage?.text || message.message.conversation) {
+  let textMessage = message.message.extendedTextMessage?.text || message.message.conversation;
+  
+  if (textMessage.length > 0) {
+    try {
+      const textoColor = chalk.red(textMessage); // Cambia a chalk.green, chalk.blue, chalk.yellow según el color que desees
+      sock.sendMessage(groupJid, { text: textoColor });
+      spinner.succeed("Texto de color enviado correctamente");
+    } catch (error) {
+      spinner.fail(`Fallo el envío de texto a color: ${error.toString()}`).start();
+     }
+   }
+ }
+};
